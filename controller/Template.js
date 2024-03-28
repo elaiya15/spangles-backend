c38af72930df73664b6c56e7f196108b9254ea49
elaiya15 <elaiyakutty143@gmail.com>
2024-03-28 18:14:16 +0530

    done

--- controller/Template.js
+++ controller/Template.js
@@ -66,7 +66,7 @@ exports.getAll = async (req, res, next) => {
         return res.status(404).json({ message:  'Template not found' });
       }
   
-      return res.status(200).json( { message:  'Template updated SuccessFull',updatedTemplate});
+      return res.status(200).json( { message:  'Template updated SuccessFull'});
     } catch (err) {
       console.error(err);
       return res.status(500).json({ message:  'Server error' });
