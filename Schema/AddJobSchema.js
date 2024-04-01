552bdd11d86c90c6455ff3f31dfb99b2efef7ac5
elaiya15 <elaiyakutty143@gmail.com>
2024-04-01 13:19:41 +0530

    addjob last commite

--- Schema/AddJobSchema.js
+++ Schema/AddJobSchema.js
@@ -6,12 +6,12 @@
 
   const AddJobSchema = new Schema({
     Designation: { type: String, trim: true }, 
-    WorkExperience: { type: Date, trim: true }, 
+    WorkExperience: { type:String , trim: true }, 
     ClosingDate: { type: String, trim: true }, 
     RecruiterMail : { type: String, trim: true }, 
     SalaryRangeFrom : { type: String, trim: true }, 
     SalaryRangeTo : { type: String, trim: true }, 
-    PreferredSkills : { type: String, trim: true }, 
+    PreferredSkills : [{ type: String}], 
     JobDescription : { type: String, trim: true }, 
   });
    
--- controller/AddJobPost.js
+++ controller/AddJobPost.js
@@ -6,9 +6,9 @@ exports.Create = async (req, res, next) => {
   try {
     const newJob = await AddJob.create(req.body);
     await newJob.save();
-    res.status(201).json(newJob);
+    return  res.status(201).json({message:"Job Added Successful"});
   } catch (err) {
-    res.status(400).json({message: err.message });
+    return  res.status(400).json({message: err.message });
   }
 };
 
@@ -16,9 +16,9 @@ exports.Create = async (req, res, next) => {
 exports.getAll = async (req, res, next) => {
   try {
     const jobs = await AddJob.find();
-    res.status(200).json(jobs);
+    return res.status(200).json({message:"Job Get Successful",JobData: jobs});
   } catch (err) {
-    res.status(500).json({message: 'Internal Server Error' });
+    return res.status(500).json({message: 'Internal Server Error' });
   }
 };
 
@@ -42,7 +42,7 @@ exports.Update = async (req, res, next) => {
     if (!updatedJob) {
       return res.status(404).json({message: 'Job not found' });
     }
-    res.status(200).json(updatedJob);
+    return res.status(200).json({message:"Update Successful"});
   } catch (err) {
     res.status(500).json({message: 'Internal Server Error' });
   }
@@ -55,7 +55,7 @@ exports.Delete = async (req, res, next) => {
     if (!deletedJob) {
       return res.status(404).json({message: 'Job not found' });
     }
-    res.status(204).end();
+    return  res.status(204).json({message: 'Deleted Successful' } );
   } catch (err) {
     res.status(500).json({message: 'Internal Server Error' });
   }
