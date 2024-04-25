ea8c50a7523736b2a87fe26eccfcf73f3d26e783
elaiya15 <elaiyakutty143@gmail.com>
2024-04-25 11:51:38 +0530

    employee-details

--- routes/joiningForm.js
+++ routes/joiningForm.js
@@ -14,7 +14,7 @@ router.put("/update/:id", Selected.updateJoiningList);
 
 //  get single joining get to sent client side
 
-router.get("/singleList/:id", Selected.SingleJoiningList);
+router.get("/employee-details/:id", Selected.SingleJoiningList);
 
 // update_Client_Joining_Form
 router.put("/clientUpdate/:id", Selected.update_Client_Joining_Form);
