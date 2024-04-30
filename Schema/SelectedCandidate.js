849051cb3cb2e0af6ced21a568c20358abe08003
elaiya15 <elaiyakutty143@gmail.com>
2024-04-30 11:47:04 +0530

    SelectedCandidateSchema

--- Schema/SelectedCandidate.js
+++ Schema/SelectedCandidate.js
@@ -4,19 +4,13 @@ const Schema = mongoose.Schema;
 // Define Mongoose schema for ApplicationList
 const SelectedCandidateSchema = new Schema({
   Applicant_id:mongoose.SchemaTypes.ObjectId,
-  Name: { type: String, required: true, trim: true },
   EmployeeCode: { type: String, trim: true },
   ProfileImage: { type: String, trim: true },
-  Designation: { type: String, required: true, trim: true },
   JoiningDate: { type: Date, trim: true },
   Gender: { type: String, enum: ['Male', 'Female', 'Other'], trim: true },
   MaritalStatus: { type: String, enum: ['Single', 'Married', 'Divorced', 'Widowed'], trim: true },
   DateofBirth: { type: String, trim: true },
   EducationQualification: {type :Array},
-  PhoneNumber: { type: String, trim: true },
-  AlternativePhoneNumber: { type: String, trim: true },
-  EmailOffice: { type: String, unique: true, trim: true },
-  EmailPersonal: { type: String, unique: true, trim: true },
   BloodGroup: { type: String, trim: true },
   AadhaarNumber: { type: String },
   AddressPresent: {
